0421c3ae5ac99f4dba55d0a434a11277b1412b48
WangYouCoder <1684025215@qq.com>
2024-02-23 12:51:44 +0800

    A

--- process1/code.c
+++ process1/code.c
@@ -2,12 +2,47 @@
 #include<unistd.h>
 int main()
 {
-    int i = 1000;
-    while(i--)
+    printf("我是一个父进程，我的pid:%d\n",getpid());
+    pid_t id =  fork();
+
+    if(id < 0) return 1;
+    else if(id == 0)
     {
-        pid_t id = getpid();
-        printf("我是一个进程...,我的pid是：%d\n",id);
-        sleep(1);
+        //child
+        while(1)
+        {
+            printf("我是子进程:pid: %d,ppid: %d,ret: %d,我正在执行下载任务,&idi:%p\n",getpid(),getppid(),id,&id);
+            sleep(1);
+        }
     }
+    else 
+    {
+        while(1)
+        {
+
+            printf("我是父进程:pid: %d,ppid: %d,ret: %d,我正在执行播放任务,&id:%p\n",getpid(),getppid(),id,&id);
+            sleep(1);
+         }
+    }
+
+
+
+//    int i = 1000;
+//    while(i--)
+//    {
+//        pid_t id = getpid();
+//        pid_t ppid = getppid();
+//        printf("I am a process pid:%d,parent id:%d\n",id,ppid);
+//        sleep(1);
+//    
+//    }
+//
+   // printf("我是一个父进程，我的pid:%d,ppid:%d\n",getpid(),getppid());
+
+   // fork();
+
+   // printf("我是一个进程，我的pid:%d,ppid:%d\n",getpid(),getppid());//注意这里只运行了一次
+
+   // sleep(1);
     return 0;
 }
---
+++ process1/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+    while(1)
+    {
+       printf("hello Linux\n");
+       sleep(1);
+    }
+
+
+
+
+
+
+   // int num = 0;
+   // printf("输入一个数字:");
+   // scanf("%d",&num);
+   // printf("echo:%d\n",num);
+   // 
+    return 0;
+
+}
