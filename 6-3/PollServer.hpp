a2852410dbd7c20dfc5505249bcf55c0df837017
WangYouCoder <1684025215@qq.com>
2024-06-03 20:17:18 +0800

    poll

--- 6-3/PollServer.hpp
+++ 6-3/PollServer.hpp
@@ -14,6 +14,7 @@ class PollServer
 private:
     void HandlerEvent()
     {
+        // std::cout << "---------------------------" << std::endl;
         for (int i = 0; i < _num; i++)
         {
             if (_rfds[i].fd == -1)
@@ -21,32 +22,37 @@ private:
             // 读事件分两类，一类是新连接到来，一类是新数据到来
             int fd = _rfds[i].fd;
             short revents = _rfds[i].revents;
+
             if (revents & POLLIN)
             {
+                // std::cout << fd << " " << _listensock->GetSockfd() << std::endl;
+                //  新连接到来了
                 if (fd == _listensock->GetSockfd())
                 {
-                    // 连接
                     lg.LogMessage(Info, "get a new link\n");
-                    // 获得连接 
+                    // 获取连接
                     std::string clientip;
                     uint16_t clientport;
+                    // 不会阻塞！！，因为select已经检测到了listensock已经就绪了
                     Socket *sock = _listensock->AccetpConnecton(&clientip, &clientport);
-                    if (sock == nullptr)
+                    if (sock->GetSockfd() == -1)
                     {
                         lg.LogMessage(Error, "accept error\n");
                         continue;
                     }
+                    lg.LogMessage(Info, "get a client, client info is# %s:%d, fd: %d\n", clientip.c_str(), clientport, sock->GetSockfd());
 
-                    lg.LogMessage(Info, "get a client, client info is: %s:%d, fd: %d\n", clientip.c_str(), clientport, sock->GetSockfd());
-
+                    // 这里已经获取连接成功了，接下来怎么办？？？
+                    // read？write？绝对不能！！！read 底层数据是否就绪时不确定的！谁清楚fd上面是否有读事件呢？poll！
+                    // 新链接fd到来的时候，要把新的fd, 想办法交给poll托管 -- 只需要添加到数组_rfds中即可
                     int pos = 0;
                     for (; pos < _num; pos++)
                     {
                         if (_rfds[pos].fd == -1)
                         {
                             _rfds[pos].fd = sock->GetSockfd();
-                            _rfds[pos].events |= POLLIN;
-                            lg.LogMessage(Info, "get a new link, fd is: %d\n", sock->GetSockfd());
+                            _rfds[pos].events = POLLIN;
+                            lg.LogMessage(Info, "get a new link, fd is : %d\n", sock->GetSockfd());
                             break;
                         }
                     }
@@ -54,47 +60,48 @@ private:
                     {
                         // 1. 扩容
                         // 2. 关闭
-                        sock->CloseSockfd();
-                        delete sock;
-                        lg.LogMessage(Warning, "server is full...\n");
+                        close(sock->GetSockfd());
+                        lg.LogMessage(Warning, "server is full...!\n");
                     }
-                }
-            }
-            else
-            {
-                // IO
-                char buffer[1024];
-                ssize_t n = recv(fd, buffer, sizeof(buffer) - 1, 0);
-                if(n > 0)
-                {
-                    buffer[n] = 0;
-                    lg.LogMessage(Info, "client say# %s\n", buffer);
-                    std::string message = "hello";
-                    message += buffer;
-                    send(fd, message.c_str(), message.size(),0);                    
                 }
                 else
                 {
-                    lg.LogMessage(Warning, "client quit, maybe close or error, close fd: %d\n", fd);
-                    close(fd);
-                    // 取消poll的关系
-                    _rfds[i].fd = -1;
-                    _rfds[i].events = 0;
-                    _rfds[i].revents = 0;
+                    // 普通的读事件就绪
+                    // 读数据是有问题的
+                    // 这一次读取不会被卡住吗？
+                    char buffer[1024];
+                    ssize_t n = recv(fd, buffer, sizeof(buffer - 1), 0); // 这里读取会阻塞吗？不会！
+                    if (n > 0)
+                    {
+                        buffer[n] = 0;
+                        lg.LogMessage(Info, "client say# %s\n", buffer);
+                        std::string message = "hello ";
+                        message += buffer;
+                        send(fd, message.c_str(), message.size(), 0);
+                    }
+                    else
+                    {
+                        lg.LogMessage(Warning, "client quit, maybe close or error, close fd : %d\n", fd);
+                        close(fd);
+                        // 取消poll的关心
+                        _rfds[i].fd = -1;
+                        _rfds[i].events = 0;
+                        _rfds[i].revents = 0;
+                    }
                 }
             }
         }
     }
 
 public:
-    PollServer(uint16_t port) : _port(port), _listensock(new TcpSocket()), _isruning(false), _num(gnum) 
+    PollServer(uint16_t port) : _port(port), _listensock(new TcpSocket()), _isruning(false), _num(gnum)
     {
     }
     void InitServer()
     {
         _listensock->BuildListenSocketMethod(_port);
         _rfds = new struct pollfd[_num];
-        for(int i = 0; i < _num; i++)
+        for (int i = 0; i < _num; i++)
         {
             _rfds[i].fd = -1;
             _rfds[i].events = 0;
@@ -110,7 +117,7 @@ public:
         while (_isruning)
         {
             // 定义时间
-            int timeout = 1000;
+            int timeout = -1;
             int n = poll(_rfds, _num, timeout);
             switch (n)
             {
@@ -121,7 +128,7 @@ public:
                 lg.LogMessage(Error, "select error...\n");
                 break;
             default:
-                lg.LogMessage(Info, "select success, begin event handler...");
+                lg.LogMessage(Info, "select success, begin event handler...\n");
                 HandlerEvent();
                 break;
             }
@@ -130,7 +137,7 @@ public:
     }
     ~PollServer()
     {
-        delete []_rfds;
+        delete[] _rfds;
     }
 
 private:
